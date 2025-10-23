3491bf29bc039f634365aac390ec12b418bceda3
self entreprenur <sanafarzana27@gmail.com>
2025-10-23 12:00:16 +0530

    first commit

---
+++ date_time.py
@@ -0,0 +1,36 @@
+
+# #Yearly Calendar Program
+# # yearly_calendar.py
+
+
+# import calendar
+
+# year = int(input("Enter year: "))
+# print("\n📅 Calendar for the year", year)
+# print(calendar.calendar(year))
+
+
+
+# #Stopwatch Program
+# # stopwatch.py
+
+# import time
+
+# print("⏱️ Stopwatch started... (Press Ctrl+C to stop)")
+# start = time.time()
+
+# try:
+#     while True:
+#         elapsed = time.time() - start
+#         print(f"Time elapsed: {elapsed:.2f} seconds", end="\r")
+#         time.sleep(0.1)
+# except KeyboardInterrupt:
+#     print("\n✅ Stopwatch stopped!")
+
+
+
+
+
+
+
+
